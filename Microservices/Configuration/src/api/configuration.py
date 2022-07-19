0dec9dc0e52f46e63448c0172cb2a3fd5169f07c
JMackie80 <jmackie17@yahoo.com>
2022-07-19 11:54:41 -0400

    update configuration service

---
+++ Microservices/Configuration/src/api/configuration.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint('configuration', __name__, url_prefix='/configuration')
--- Microservices/Configuration/src/app.py
+++ Microservices/Configuration/src/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from .api import configuration
 
 application = Flask(__name__)
 
+import models
+application.register_blupring(configuration.bp)
+
 @application.route("/", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Configuration Running</p>"
