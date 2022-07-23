3c9580f84134ce7ad13472354e896d318eb28416
JMackie80 <jmackie17@yahoo.com>
2022-07-23 13:14:04 -0400

    update gatekeeper

--- Microservices/Gatekeeper/src/api/apiproxy.py
+++ Microservices/Gatekeeper/src/api/apiproxy.py
@@ -3,19 +3,7 @@ from requests import get, put, post, delete
 
 CONFIGURATION_URL = "https://configuration-service-jmackie80.cloud.okteto.net/"
 
-"""
-sites = {
-    "availability": "https://availability-service-jmackie80.cloud.okteto.net/",
-    "configuration": "https://configuration-service-jmackie80.cloud.okteto.net/",
-    "employee": "https://employee-service-jmackie80.cloud.okteto.net/",
-    "scheduling": "https://scheduling-service-jmackie80.cloud.okteto.net/",
-    "calendar": "https://calendar-service-jmackie80.cloud.okteto.net/",
-    "communications": "https://communications-service-jmackie80.cloud.okteto.net/"
-}
-"""
-
-site_configuration = get(f'{CONFIGURATION_URL}configuration/sites').json()
-sites = site_configuration['configurationValue']
+sites = None
 
 bp = Blueprint('apiproxy', __name__, url_prefix='/api')
 
@@ -26,6 +14,7 @@ def proxyRoot():
 @bp.route('/<path:api>/', defaults={'path': ''}, methods=['GET', 'PUT', 'POST', 'DELETE'])
 @bp.route('/<path:api>/<path:path>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def proxy(api, path):
+  getSites()
   if request.method == 'GET':
     return get(f'{sites[api]}{path}').content
   if request.method == 'PUT':
@@ -36,3 +25,10 @@ def proxy(api, path):
     return delete(f'{sites[api]}{path}').content
   else:
     return "<p>unknown request</p>"
+
+def getSites():
+  global sites
+  if sites is None:
+    site_configuration = get(f'{CONFIGURATION_URL}configuration/sites').json()
+    sites = site_configuration['configurationValue']
+  return sites
