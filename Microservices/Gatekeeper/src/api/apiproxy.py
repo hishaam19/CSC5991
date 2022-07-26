780722f675a84bba67ee3cd5b6a881ad673f0ffc
JMackie80 <jmackie17@yahoo.com>
2022-07-26 13:04:00 -0400

    add authentication and authorization

--- Microservices/Gatekeeper/src/api/apiproxy.py
+++ Microservices/Gatekeeper/src/api/apiproxy.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from requests import get, put, post, delete
 
 CONFIGURATION_URL = "https://configuration-service-jmackie80.cloud.okteto.net/"
@@ -15,26 +15,35 @@ def proxyRoot():
 @bp.route('/<path:api>/', defaults={'path': ''}, methods=['GET', 'PUT', 'POST', 'DELETE'])
 @bp.route('/<path:api>/<path:path>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def proxy(api, path):
-  # authorize user
-  security_response = get(f'{CONFIGURATION_URL}authorize', headers= {'Authorization': request.headers.get('Authorization')})
-  status_code = security_response.status_code
-  if status_code == 403:
-    return Response('Access Denied', 403)
-  if status_code == 401 or status_code >= 300 or status_code < 200:
-    return Response('Not Authenticated', 401)
-  user = security_response.json()
-  headers = {'USER_NAME': user['userName'], 'USER_SESSION_ID': user['sessionId'], 'USER_FULL_NAME': user['fullName'], 'USER_EMAIL': user['email']}
+  full_path = f'{api}/{path}'
+  headers = None
+  print(full_path)
+  if full_path != "security/login" and full_path != "security/register":
+    # authorize user
+    security_response = post(f'{CONFIGURATION_URL}authorize', data=jsonify({ 'destination': path }), headers={'Authorization': request.headers.get('Authorization')})
+    status_code = security_response.status_code
+    if status_code == 403:
+      return Response('Access Denied', 403)
+    if status_code == 401 or status_code >= 300 or status_code < 200:
+      return Response('Not Authenticated', 401)
+    user = security_response.json()
+    headers = {'USER_NAME': user['userName'], 'USER_SESSION_ID': user['sessionId'], 'USER_FULL_NAME': user['fullName'], 'USER_EMAIL': user['email']}
 
   # forward to internal service
   getSites()
+  print(sites)
+  if api == "security":
+    site_path = f'{SECURITY_URL}{path}'
+  else:
+    site_path = f'{sites[api]}{path}'
   if request.method == 'GET':
-    return get(f'{sites[api]}{path}', headers=headers).content
+    return get(site_path, headers=headers).content
   if request.method == 'PUT':
-    return put(f'{sites[api]}{path}', data=request.get_json(), headers=headers).content
+    return put(site_path, data=request.get_json(), headers=headers).content
   if request.method == 'POST':
-    return post(f'{sites[api]}{path}', data=request.get_json(), headers=headers).content
+    return post(site_path, data=request.get_json(), headers=headers).content
   if request.method == 'DELETE':
-    return delete(f'{sites[api]}{path}', headers=headers).content
+    return delete(site_path, headers=headers).content
   else:
     return "<p>unknown request</p>"
 
