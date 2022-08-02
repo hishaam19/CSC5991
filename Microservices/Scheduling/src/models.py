1dc315f3e961bb1ba35f42338d6a7ee4b81d1187
BorjanaVeliu <90228043+BorjanaVeliu@users.noreply.github.com>
2022-08-02 11:23:30 -0400

    Update models.py

--- Microservices/Scheduling/src/models.py
+++ Microservices/Scheduling/src/models.py
@@ -2,13 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Scheduling(db.Model):
+class Appointment(db.Model):
     __tablename__ = 'scheduling'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    company = db.relationship('User', foreign_keys=[company_id], backref=db.backref('company', lazy=True))
+    client = db.relationship('User', foreign_keys=[client_id], backref=db.backref('client', lazy=True))
 
- id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(USERNAME_LEN_MAX), nullable=False)
-    email = db.Column(db.String(EMAIL_LEN_MAX), nullable=False)
-    start_time = db.Column(db.Integer, nullable=False)
-    end_time = db.Column(db.Integer, nullable=False)
-    timezone = db.Column(db.String(TIMEZONE_LEN_MAX), nullable=False)
-    message = db.Column(db.String(MESSAGE_LEN_MAX), nullable=False)
+    def __repr__(self):
+        return self.title
+
+db.create_all()
