2e25946f504da0502206552e8f6b29567b7b9b67
JMackie80 <jmackie17@yahoo.com>
2022-08-02 19:49:27 -0400

    update scheduling service

--- Microservices/Scheduling/src/models.py
+++ Microservices/Scheduling/src/models.py
@@ -14,7 +14,6 @@ class Interview(db.Model):
     def serialize(self):
         return {
             'id': self.id,
-<<<<<<< HEAD
             'durationinminutes': self.durationinminutes,
             'recruiterusername': self.recruiterusername,
             'candidateusername': self.candidateusername,
@@ -27,44 +26,10 @@ class UserInterview(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     interviewid = db.Column(db.Integer, nullable=False)
     username = db.Column(db.String(128), nullable=False)
-=======
-            'fullName': self.fullname,
-            'userName': self.username,
-            'password': self.password,
-            'email': self.email,
-            'sessionId': self.sessionid,
-            'role': self.role
-        }
-
-class Scheduling (db.Model):
-    __tablename__ = 'scheduling'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullabl=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-    end_time = db.Column(db.DateTime, nullable=False)
-    is_active = db.Column(db.Boolean, nullable=False, default=True)
-    company_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    client_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    company = db.relationship('User', foreign_keys=[company_id], backref=db.backref('company', lazy=True))
-    client = db.relationship('User', foreign_keys=[client_id], backref=db.backref('client', lazy=True))
->>>>>>> 6c5f7d004e094ad63eda33303ce2ec258edc0d04
 
     def serialize(self):
         return {
             'id': self.id,
-<<<<<<< HEAD
             'interviewid': self.interviewid,
             'username': self.username
         }
-=======
-            'title': self.title,
-            'start_time': self.start_time,
-            'end_time': self.end_time,
-            'is_active': self.is_active,
-            'company_id': self.company_id,
-            'client_id': self.client_id,
-            'company': self.company,
-            'client': self.client            
-        }
-
->>>>>>> 6c5f7d004e094ad63eda33303ce2ec258edc0d04
