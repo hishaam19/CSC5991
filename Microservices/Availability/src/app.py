7d85ddc5cfadafcda0d045a5e61c3db23807a202
Hishaam Ahmad <66968397+hishaam19@users.noreply.github.com>
2022-08-03 10:01:41 -0400

    Update app.py

--- Microservices/Availability/src/app.py
+++ Microservices/Availability/src/app.py
@@ -1,21 +1,5 @@
 from flask import Flask
 
-from api import availability
-
-application = Flask(__name__)
-
-application.config.from_mapping(
-    SECRET_KEY='csc5991',
-    SQLALCHEMY_DATABASE_URI="postgresql://okteto:okteto@10.152.137.106:5432/Availability",
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    SQLALCHEMY_ECHO=True
-)
-
-application.register_blueprint(availability.bp)
-
-from models import db
-db.init_app(application)
-
 @application.route("/", methods=['GET'])
 def hello_world():
-    return "<p>Availability Service Running</p>"
+    return "<p>-</p>"
